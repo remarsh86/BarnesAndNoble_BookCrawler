94db49ebe174faf4ccb9c138ee8a3dc77b61d809
remarsh86 <rebecca.marsh711@gmail.com>
2019-07-02 11:19:56 +0200

    Non-recursive crawler

--- scraper.py
+++ scraper.py
@@ -7,16 +7,18 @@ from urllib.request import urlopen
 import parser as ps
 import re
 
-class scraper():
 
 
+class Scraper():
 
     scraped_urls=[]
     isbns=[]
-    #counter =0
-    def scrape(self, url ):
 
-        url = url
+
+    def scrape(self, url):
+        print("scraped urls: ", self.scraped_urls[:5])
+        self.scraped_urls.append(url)
+        #url = url
         #Avoid 403 Errors
         headers = requests.utils.default_headers()
         headers.update({
@@ -52,41 +54,40 @@ class scraper():
             except:
                 print("curious, no isbn ")
 
-
+        # print("how many links")
+        # print(content.find_all("a", href=True))
+        found_urls = []
         #scrape other urls on page:
         for item in content.find_all("a", href=True):
             #avoid cycles
-            url2 = item['href']
-            #print(url2)
-            url2 = urljoin(url, url2)  # Actual URL
-            print("URL2!!!!!!!: ", url2)
-            #scraped_urls.append(url2)
-            if url2 in self.scraped_urls:
+            relative_url = item['href']
+            full_url = urljoin(url, relative_url)  # Actual URL
+            print("URL2!!!!!!!: ", full_url)
+
+
+            if full_url in self.scraped_urls:
                 print("already visited")
-            else:
-                self.scraped_urls.append(url2)
-                # url2 = urljoin(url, url2) #Actual URL
-                # print("URL2!!!!!!!: ", url2)
-                # scraped_urls.append(url2)
-                if str(url2).startswith("https://www.barnesandnoble.com/w/"):
-                    # if "ean" in str(url):
-                    #     print("Parse book info!")
-                    #     ps.parse_book_info(content)
-                    # else:
-                    #     print("follow ", url2)
-                    #     self.scrape(url2)
-                     print("follow ", url2)
-                     self.scrape(url2)
+            elif str(full_url).startswith("https://www.barnesandnoble.com/"):
+                #     print("Add url to list", full_url)
+                #     #self.scrape(url2)
+                #     found_urls.append(full_url)
+                print("apend to list")
+                found_urls.append(str(full_url))
+        print("what is in list found_urls?", found_urls)
+        return found_urls
 
 
 #start scraper
-#scrape('http://books.toscrape.com/index.html')
+links_todo = ['https://www.barnesandnoble.com/']
+scraper = Scraper()
+while(links_todo):#until list is emtpy, keep crawling
+    url = links_todo.pop() #returns last element and removes that element from the list
+    new_urls = scraper.scrape(url)
+    links_todo += new_urls
 
-scraper = scraper()
-scraper.scrape('https://www.barnesandnoble.com/')
-print("!!!!!!!!!!!!!!!")
-for x in scraper.scraped_urls:
-    print(x)
+
+    print("!!!!!!!!!!!!!!!")
+    print("links to do: ", links_todo)
 
 
 
