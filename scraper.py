4cedf2624c57767af11e62993886c4af8110dd23
remarsh86 <rebecca.marsh711@gmail.com>
2019-07-02 09:58:51 +0200

    Crawler is working but crashes because recursion idea is not robust.

--- scraper.py
+++ scraper.py
@@ -5,66 +5,87 @@ import requests
 from urllib.parse import urljoin
 from urllib.request import urlopen
 import parser as ps
+import re
 
-scraped_urls=[]
-#counter =0
-def scrape(url ):
+class scraper():
 
-    url = url #relativ URL
-    #Avoid 403 Errors
-    headers = requests.utils.default_headers()
-    headers.update({
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-    })
-    response = requests.get(url,  headers)
-    raw_html = response.content
-    content = BeautifulSoup(raw_html, "html.parser")
 
-    #scrape other urls on page:
-    for item in content.find_all("a", href=True):
-        #avoid cycles
-        url2 = item['href']
-        print(url2)
-        if url2 in scraped_urls:
-            print()
-        else:
-            scraped_urls.append(url2)
-            url2 = urljoin(url, url2) #Actual URL
-            print("URL2!!!!!!!: ", url2)
-            if str(url2).startswith("https://www.barnesandnoble.com/w/"):
-                if "ean" in str(url):
+
+    scraped_urls=[]
+    isbns=[]
+    #counter =0
+    def scrape(self, url ):
+
+        url = url
+        #Avoid 403 Errors
+        headers = requests.utils.default_headers()
+        headers.update({
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        })
+        response = requests.get(url,  headers)
+        raw_html = response.content
+        content = BeautifulSoup(raw_html, "html.parser")
+
+        print("What is this url? ", url)
+        if "ean=" in str(url):
+            try:
+                isbn = re.search(r'ean=([0-9]{13})', str(url)).group(1)
+                if isbn not in self.isbns:
+                    self.isbns.append(isbn)
                     print("Parse book info!")
                     ps.parse_book_info(content)
                 else:
-                    print("follow ", url2)
-                    scrape(url2)
-#
-# def parse_book_info(content):
-#     product_table = content.find_all("table", class_="plain centered")
-#     print(product_table)
-#
-#     table_rows = product_table[0].find('tbody').findAll('tr')
-#     item = {}
-#     for row in table_rows:
-#         print()
-#         # print(row)
-#         th = row.find('th').contents[0]
-#         print("th: ", th)
-#         if "Publisher" in th:
-#             td = row.find('td').text
-#             td = td.strip('\n')  # remove trailing new line
-#         else:
-#             td = row.find('td').contents[0]
-#         print("td: ", td)
-#         item[th]=td
+                    print("already parsed that book")
+            except:
+                print("curious, no isbn ")
+        else:
+            try:
+                #isbn = re.search(r'([0-9]{13})', str(url)).group(1)
+                isbn = re.search(r'([0-9]{10})', str(url)).group(1)
 
+                if isbn not in self.isbns:
+                    self.isbns.append(isbn)
+                    print("Parse book info!")
+                    ps.parse_book_info(content)
+                else:
+                    print("already parsed that book")
+            except:
+                print("curious, no isbn ")
+
+
+        #scrape other urls on page:
+        for item in content.find_all("a", href=True):
+            #avoid cycles
+            url2 = item['href']
+            #print(url2)
+            url2 = urljoin(url, url2)  # Actual URL
+            print("URL2!!!!!!!: ", url2)
+            #scraped_urls.append(url2)
+            if url2 in self.scraped_urls:
+                print("already visited")
+            else:
+                self.scraped_urls.append(url2)
+                # url2 = urljoin(url, url2) #Actual URL
+                # print("URL2!!!!!!!: ", url2)
+                # scraped_urls.append(url2)
+                if str(url2).startswith("https://www.barnesandnoble.com/w/"):
+                    # if "ean" in str(url):
+                    #     print("Parse book info!")
+                    #     ps.parse_book_info(content)
+                    # else:
+                    #     print("follow ", url2)
+                    #     self.scrape(url2)
+                     print("follow ", url2)
+                     self.scrape(url2)
 
 
 #start scraper
 #scrape('http://books.toscrape.com/index.html')
-scrape('https://www.barnesandnoble.com/')
+
+scraper = scraper()
+scraper.scrape('https://www.barnesandnoble.com/')
 print("!!!!!!!!!!!!!!!")
-for x in scraped_urls:
+for x in scraper.scraped_urls:
     print(x)
 
 
