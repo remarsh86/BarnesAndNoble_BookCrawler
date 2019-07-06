d161b89e7f47b203b6a7560b65340cd762e9e4b3
remarsh86 <rebecca.marsh711@gmail.com>
2019-07-06 23:21:32 +0200

    version with relative delay seemed to have impossibly long delay

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from urllib.parse import urljoin
 from urllib.request import urlopen
 import parser as ps
 import re
+import time
 
 
 
@@ -16,12 +17,14 @@ class Scraper():
 
 
     def scrape(self, url, links_todo):
+        self.pages_visited.append(url)
+
         print("scraped urls: ", self.pages_visited)
         print("links_todo: ", links_todo)
         for x in self.pages_visited:
             if x in links_todo:
                 print("oh no: the following book is in linked_todo and scraped_urls: ", x)
-        self.pages_visited.append(url)
+
         #url = url
         #Avoid 403 Errors
         headers = requests.utils.default_headers()
@@ -60,7 +63,7 @@ class Scraper():
 
         # print("how many links")
         # print(content.find_all("a", href=True))
-        found_urls = []
+
         #scrape other urls on page:
         for item in content.find_all("a", href=True):
             #avoid cycles
@@ -73,26 +76,38 @@ class Scraper():
                 print("already visited")
             elif str(full_url).startswith("https://www.barnesandnoble.com/w/"):
 
-                if (str(full_url) not in links_todo) and (str(full_url) not in found_urls) and ((len(links_todo)+len(found_urls))<20000):
-                    print("apend to list")
-                    found_urls.append(str(full_url))
-        print("what is in list found_urls?", found_urls)
-        return found_urls
+                if (str(full_url) not in links_todo)  and ((len(links_todo)+1)<1000):
+                    #print("apend to list")
+                    #found_urls.append(str(full_url))
+                    links_todo.append(str(full_url))
+
+        return links_todo
 
 
 #start scraper
 links_todo = ['https://www.barnesandnoble.com/']
 scraper = Scraper()
-while(links_todo and (len(links_todo)<20000)):#until list is emtpy, keep crawling
+while(links_todo and (len(links_todo)<1000)):#until list is emtpy, keep crawling
     print("length of links_todo: ", len(links_todo))
-    url = links_todo.pop() #returns last element and removes that element from the list
+    #url = links_todo.pop() #returns last element and removes that element from the list
+    url = links_todo.pop(0)  # returns first element and removes that element from the list
     print("length of links_todo after pop: ", len(links_todo))
+
+    #back off from server if delays pop up
+    #t0 = time.time()
+
     try:
-        new_urls = scraper.scrape(url, links_todo)
-        links_todo += new_urls
+        print("parse this page: ", url)
+        # new_urls = scraper.scrape(url, links_todo)
+        # links_todo += new_urls
+        links_todo = scraper.scrape(url, links_todo)
+
     except:
         print("Connection error")
 
+    time.sleep(5)
+    # response_delay = time.time() -t0
+    # time.sleep(10*response_delay)
 
     # print("!!!!!!!!!!!!!!!")
     # print("links to do: ", links_todo)
